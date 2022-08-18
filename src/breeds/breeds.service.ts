ffb34db7c11e5adaecb380fdc0454194594455fc
Daniel Kamasi <scientificallychristian@gmail.com>
2022-08-18 14:09:22 +0700

    Mongodb CRUD using mongoose

---
+++ src/breeds/breeds.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Breed, BreedDocument } from 'src/schemas/breed.schema';
+import { CreateBreedDto } from './dto/create-breed.dto';
+import { UpdateBreedDto } from './dto/update-breed.dto';
+
+@Injectable()
+export class BreedsService {
+  constructor(
+    @InjectModel(Breed.name) private breedModel: Model<BreedDocument>,
+  ) {}
+  create(createBreedDto: CreateBreedDto): Promise<Breed> {
+    const createdBreed = new this.breedModel(createBreedDto);
+    return createdBreed.save();
+  }
+
+  findAll() {
+    return this.breedModel.find();
+  }
+
+  findOne(name: string) {
+    return this.breedModel.findOne({ name });
+  }
+
+  update(name: string, updateBreedDto: UpdateBreedDto) {
+    return this.breedModel.updateOne({ name }, { $set: { ...updateBreedDto } });
+  }
+
+  remove(name: string) {
+    return this.breedModel.deleteOne({ name });
+  }
+}
